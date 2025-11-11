9bdd9dc7eeffaf16f97423ca8a69f0f155c964da
luisfnima <luisf.nima@gmail.com>
2025-11-11 14:55:03 -0500

    Cambios

--- crm-call-center/src/components/layout/MainLayout.jsx
+++ crm-call-center/src/components/layout/MainLayout.jsx
@@ -4,13 +4,13 @@ import Sidebar from './Sidebar';
 import Topbar from './Topbar';
 
 const MainLayout = () => {
-    const [sidebarOpen, setsidebarOpen] = useState(true);
+    const [sidebarOpen, setSidebarOpen] = useState(true);
     return (
         <div className='flex h-screen bg-gray-50'>
-            <Sidebar isOpen={sidebarOpen} setIsOpen={setsidebarOpen} />
+            <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
 
             <div className='flex-1 flex flex-col overflow-hidden'>
-                <Topbar toggleSidebar={() => setsidebarOpen(!isSidebarOpen)} />
+                <Topbar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
                 <main className='flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-6'>
                     <Outlet />
                 </main>
