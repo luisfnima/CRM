6e25d899613e413c0498aa12878b3cad1e68d60e
luisfnima <luisf.nima@gmail.com>
2025-11-11 14:59:04 -0500

    Cambios

--- crm-call-center/tailwind.config.js
+++ crm-call-center/tailwind.config.js
@@ -5,17 +5,7 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {
-            colors: {
-                primary: {
-                    50: '#eff6ff',
-                    100: '#dbeafe',
-                    500: '#3b82f6',
-                    600: '#2563eb',
-                    700: '#1d4ed8',
-                }
-            }
-        },
+        extend: {},
     },
     plugins: [],
 }
