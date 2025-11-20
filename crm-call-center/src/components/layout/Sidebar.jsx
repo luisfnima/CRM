0f0f9de159edae69c1bc72ada30c4ff5792df458
luis-f-nima <luisf.nima@gmail.com>
2025-11-20 07:33:11 -0500

    se sube temporalmente para continuar desde la PC

--- crm-call-center/src/components/layout/Sidebar.jsx
+++ crm-call-center/src/components/layout/Sidebar.jsx
@@ -57,19 +57,19 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
             section: 'Mi Empresa',
             key: 'company',
             items: [
-                { name: 'Inicio', icon: LayoutDashboard, path: '/company/start' },
+                { name: 'Inicio', icon: LayoutDashboard, path: '/company/main' },
                 { name: 'Config de Empresa', icon: Building2, path: '/company/company-config' },
                 {
                     name: 'Config. de Asistencia',
                     icon: Clock,
                     subItems: [
                         { name: 'Horarios', icon: LayoutDashboard, path: '/company/attendance/schedules'},
-                        { name: 'Tipos de Conexion', icon: 'Wifi', path: '/company/attendance/disconection-types'},
+                        { name: 'Tipos de Conexión', icon: 'Wifi', path: '/company/attendance/disconection-types'},
                         { name: 'Sede', icon: MapPin, path: '/company/attendance/branches' }
                     ]
                 },
                 {
-                    name: 'Administracion de Usuarios',
+                    name: 'Administración de Usuarios',
                     icon: Users,
                     subItems: [
                         { name: 'Usuarios', icon: Users, path: '/company/users'},
@@ -91,19 +91,77 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                         { name: 'Asistencia', icon: Clock, path: '/company/reports/attendance'},
                         { name: 'Acceso al Sistema', icon: LogIn, path: '/company/reports/access'},
                         { name: 'Movimientos por Usuario', icon: Activity, paath: '/company/reports/sessions'},
-                        { name: 'Navegacion por Usuario', icon: Navigation, path: '/company/reports/user-navigation'},
+                        { name: 'Navegación por Usuario', icon: Navigation, path: '/company/reports/user-navigation'},
                         { name: 'Chats Iniciados', icon: MessageCircle, path: '/company/reports/chats'}
                     ]
                 }
             ]
         },
         {
-            section: 'Sales',
+            section: 'Ventas',
+            key: 'sales',
             items: [
-                { name: 'Dashboard', icon: TrendingUp, path: '/sales/dashboard' },
-                { name: 'Leads', icon: UserPlus, path: '/sales/leads' },
-                { name: 'Campaigns', icon: Megaphone, path: '/sales/campaigns' },
-                { name: 'Calls', icon: Phone, path: 'sales/calls' },
+                { name: 'Inicio', icon: TrendingUp, path: '/sales/main'},
+                { name: 'Gestion de Ventas', icon: Target, path: '/sales/sales'},
+                {
+                    name: 'Gestion de Llamadas',
+                    icon: Phone,
+                    subitems:[
+                        { name: 'Llamadas Manuales', icon: Phone, path: '/sales/calls/manual'},
+                        { name: 'Llamadas en Progresivo', icon: Headphones, path: '/sales/calls/progressive'},
+                        { name: 'Llamadas en Predictivo', icon: Radio, path: '/sales/calls/predictive'},
+                        { name: 'Monitoreo de Llamadas', icon: Eye, path: '/sales/calls/online-report'},
+                        { name: 'Campañas Telefónicas (Predictivo)', icon: Radio, path: '/sales/calls/leads-campaigns'},
+                        { name: 'Configuración Telefónica', icon: SettingsIcon, path: '/sales/calls/config'}
+                    ]
+                },
+                {
+                    name: 'Configuracion de Campañas',
+                    icon: Settings,
+                    subItems: [
+                        { name: 'Campañas', icon: Target, path: '/sales/campaign-config/campaigns'},
+                        { name: 'Pestañas de Estados', icon: Layers, path: '/sales/campaign-config/status-tab'},
+                        { name: 'Estados', icon: Grid, path: '/sales/campaign-config/status'},
+                        { name: 'Bloques de Campos', icon: Layers, path: '/sales/campaign-config/fields-blocks'},
+                        { name: 'Campos', icon: Grid, path: '/sales/campaign-config/fields'},
+                        { name: 'Categorías', icon: Tag, path: '/sales/campaign-config/categories'},
+                        { name: 'Productos', icon: Package, path: '/sales/campaign-config/products'},
+                        { name: 'Promociones', icon: Percent, path: '/sales/campaign-config/promotions'},
+                        { name: 'Supervisores', icon: UserCog, path: '/sales/campaign-config/supervisors'},
+                        { name: 'Agentes', icon: UsersIcon, path: '/sales/campaign-config/agents'},
+                        { name: 'Back Office / Jefes de Equipo', icon: Briefcase, path: '/sales/campaign-config/backoffices'},
+                        { name: 'Config. de Proyecciones de Venta', icon: TrendingUp, path: '/sales/campaign-config/projections' },
+                        { name: 'Listas de Leads', icon: List, path: '/sales/campaign-config/lead-lists' },
+                        { name: 'Listas Negras', icon: Ban, path: '/sales/campaign-config/robinson-lists' },
+                        { name: 'Tipos de Tipificaciones', icon: Tag, path: '/sales/campaign-config/result-types'},
+                        { name: 'Tipificaciones', icon: CheckSquare, path: '/sales/campaign-config/results' }
+                    ]
+                },
+                {
+                    name: 'Reportes',
+                    icon: BarChart,
+                    subItems: [
+                        { name: 'Estados de Venta', icon: Grid, path: '/sales/reports/status' },
+                        { name: 'Ranking de Vendedores', icon: Award, path: '/sales/reports/ranking' },
+                        { name: 'Consolidado de Ventas', icon: BarChart, path: '/sales/reports/consolidated' },
+                        { name: 'Monitoreo Diario de Ventas', icon: Calendar, path: '/sales/reports/sales-monitoring' },
+                        { name: 'Monitoreo BackOffice', icon: Briefcase, path: '/sales/reports/backoffice-monitoring' },
+                        { name: 'Llamadas Tipificadas', icon: PhoneCall, path: '/sales/reports/calls'},
+                        { name: 'Grabaciones por Dia', icon: Mic, path: '/sales/reports/recordings'},
+                        { name: 'Grabaciones por Telefono', icon: Phone, path: '/sales/reports/recordings-phone'},
+                        { name: 'Resúmen de Duracion de Audios', icon: Clock3, path: '/sales/reports/recordings-duration-summary'},
+                        { name: 'Resumen Tipificaciones por Lista', icon: List, path: '/sales/reports/result-list-summary'},
+                        { name: 'Búsqueda de Leads Gestionados', icon: Search, path: '/sales/reports/leads-search'},
+                        { name: 'Leads por Lista', icon: Database, path: '/sales/reports/leads-per-list'},
+                        { name: 'Tiempos de Gestión Telefónica', icon: Clock, path: '/sales/reports/call-times'},
+                        { name: 'Tiempo Agente sin Vender', icon: Clock3, path: '/sales/reports/agent-without-selling'},
+                        { name: 'Ventas Contabilizadas por Campos', icon: Grid, path: '/sales/reports/fields-grouped'},
+                        { name: 'Calendarios de Leads Gestionados', icon: Calendar, path: '/sales/reports/leads-calendar' },
+                        { name: 'Interaccion de Ventas', icon: Activity, path: '/sales/reports/sales-interaction' },
+                        { name: 'Log Exportados de Ventas', icon: Download, path: '/sale/reports/export-sales-log'},
+                        { name: 'Proyecciones de Ventas', icon: PieChart, path: '/sales/reports/'}
+                    ]
+                }
             ]
         },
     ]
